de575824ec00d787bb7c2e1e6316b5005e424503
Azhar <94859739+Azhar-Mahmood@users.noreply.github.com>
2026-02-26 10:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-	 return str.toLowerCase().indexOf(subStr.toLowerCase());
-}
+function indexOfIgnoreCase(str: string, subStr: string): number {
+    // If subStr is empty, return 0 (same as default indexOf behavior)
+    if (subStr.length === 0) return 0;
+
+    // If str is empty but subStr is not, return -1
+    if (str.length === 0) return -1;
+
+    // Convert both to lowercase for case-insensitive comparison
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+
+    return lowerStr.indexOf(lowerSubStr);
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
